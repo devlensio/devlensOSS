3e8d4e4d14e30a7275c8387f1e45db5007bdfbad
Shivang2607 <shivangkh26@gmail.com>
2026-06-20 13:30:15 +0530

    engine imports from devlensio library, engine folder removed, server added in the src folder

--- engine/src/storage/index.ts
+++
@@ -1,8 +0,0 @@
-import { fileStorage } from "./fileStorage";
-import type { GraphStorage } from "./interface";
-
-export const storage: GraphStorage = fileStorage;
-
-// Re-export types so consumers only need to import from storage/index
-export type { GraphIndexEntry, GraphMeta, CommitData, NodeDiff } from "./fileStorage";
-export type { GraphStorage } from "./interface";
